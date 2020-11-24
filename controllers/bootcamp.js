73e27ca52d562252fd4395c918932355bf9a1f64
Josh Hofstad <hofst038@gmail.com>
2020-11-24 17:44:42 +0100

    Completed fetchinng the bootcamps with GET

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -3,16 +3,32 @@ const Bootcamp = require('../models/Bootcamp');
 //@description    Get all getBootcamps
 //@route   GET api/v1/bootcamps
 //@access   Public
-exports.getBootcamps = (req, res, next) => {
-    console.log(req.body);
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({ success: true, data: bootcamps });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
+    
 };
 
 //@description    Get sinngle getBootcamp
 //@route   GET api/v1/bootcamps/:id
 //@access   Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if(!bootcamp) { // this handles an issue where the address is formatted correctly but the data doesnt exist
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 };
 
 //@description    Create new bootcamp
